9b8630b94ac0ecaf9ad50c16d8d15e668049c0ce
rayyalfawazy <rayyalfawazy@users.noreply.github.com>
2021-11-05 19:01:48 +0700

    Changelog 1.1

--- UTS_Nanar_Duplikasi/Form1.Designer.vb
+++ UTS_Nanar_Duplikasi/Form1.Designer.vb
@@ -39,7 +39,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(259, 85)
+        Me.ClientSize = New System.Drawing.Size(259, 78)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
