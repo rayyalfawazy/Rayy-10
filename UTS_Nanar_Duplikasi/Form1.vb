a9f39ad52b3b959e3fd7dc990ae7f7e9eb7ca7d9
rayyalfawazy <rayyalfawazy@users.noreply.github.com>
2021-11-08 12:35:11 +0700

    Update Rayyan Naba Ngepull

--- UTS_Nanar_Duplikasi/Form1.vb
+++ UTS_Nanar_Duplikasi/Form1.vb
@@ -10,4 +10,9 @@
     Private Sub Duplikasi_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
 End Class
